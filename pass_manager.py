5885edb18a79517a5aed0e0b4b255fe139e92df0
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-11 09:30:01 -0800

    added the logo file, added the todo list

---
+++ pass_manager.py
@@ -0,0 +1,25 @@
+# Name: Ruben Sanduleac
+# Description:
+
+# ------------------------------------ TODO: UI ---------------------------------------
+# TODO: develop a window for the program
+# TODO: implement a canvas for the logo
+# TODO: link canvas logo to the window
+# TODO: develop a text input column for typing the website that will be used to save the password
+# TODO: implement an input column for the email address/username that
+# TODO: implement an input column for password
+# TODO: make sure the password field is hidden
+# TODO: implement a button next to the password generate a new password
+# TODO: implement a button on the bottom to save the password externally --> if possible to text
+
+# ------------------------------------ TODO: SAVE PASSWORD ---------------------------------------
+# TODO: implement a function responsible for text input column for typing the website
+# TODO: implement a function responsible for input column for the email address/username
+# TODO: implement a function responsible for input column for the password field
+# TODO: implement a function responsible for "generate new password" button
+# TODO: implement a function responsible for "add" button
+
+# ---------------------------- TODO: PASSWORD GENERATOR LOGIC ------------------------------- #
+# TODO: implement a function responsible for creating a password with a preset strength
+
+
