e11e9a75b52820c4ed138af25ebc9ba24dca3a33
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-12 22:27:54 -0800

    added a few comments

--- main_manager.py
+++ main_manager.py
@@ -1,19 +1,25 @@
-from window_ui import *
-
 # Name: Ruben Sanduleac
-# Description:
+# Description: The main manager is the entry point for the password manager.
+#              the main functions initializes all the labels, buttons and the canvas.
+#              The main window passes the web_entry, username and password.
+
+from window_ui import *
 
 WINDOW_NAME = "Password Manager"
 
 
 def main():
+    # generate a window using the tkinter class
     window = Tk()
+    # change the name of the window
     window.title(WINDOW_NAME)
+    # set the size pf tje padding between canvas and the windows
     window.config(padx=20, pady=20)
     # canvas for the logo
     canvas = Canvas(width=200, height=200)
     # logo to the window
     logo = PhotoImage(file="img/logo.png")
+    # add the image to the window
     canvas.create_image(75, 100, image=logo)
     # canvas.config(bg="blue")
     canvas.grid(column=1, row=0, columnspan=2)
@@ -21,6 +27,7 @@ def main():
     web_entry = Entry()
     username_entry = Entry()
     password_entry = Entry()
+    # add the labels to the window
     main_window_labels()
     # configure the location of entries in the
     entry_config(web_entry, username_entry, password_entry)
--- password.py
+++ password.py
@@ -1,19 +1,31 @@
 # Name: Ruben Sanduleac
 # Description:
+import pyperclip
+import json
 from tkinter import *
 from tkinter import messagebox
-import pyperclip
 from random import choice, randint, shuffle
-import json
 
 
 def clear_password(web_entry, username_entry, password_entry):
+    """[This function clears everything in the gui after password is saved to the json]
+    Args:
+        web_entry ([object]): [user entry object for the website title.]
+        username_entry ([object]): [user entry object for the username.]
+        password_entry ([object]): [user entry object for the password.]
+    """
+    # delete the strings from the objects
     web_entry.delete(0, END)
     username_entry.delete(0, END)
     password_entry.delete(0, END)
 
 
 def generate_password(password_entry):
+    """[This function uses a preset lists with the whole alphabet, integers 0-9 and most common symbols
+        needed for a password. The program, then randomly picks the needed characters and shuffles the list]
+        Args:
+            password_entry ([object]): [user entry object for the password.]
+    """
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -22,19 +34,32 @@ def generate_password(password_entry):
 
     password_entry.delete(0, END)
 
+    # randomly choose between 8-10 letters, 2-4 symbols and 2-4 numbers
     password_letters = [choice(letters) for i in range(randint(8, 10))]
     password_symbols = [choice(symbols) for i in range(randint(2, 4))]
     password_numbers = [choice(numbers) for i in range(randint(2, 4))]
 
+    # concatenate password
     password_list = password_letters + password_symbols + password_numbers
+    # shuffle the order randomly
     shuffle(password_list)
-
     password = "".join(password_list)
     password_entry.insert(0, password)
+    # place the password in the clipboard
     pyperclip.copy(password)
 
 
 def find_password(web_entry):
+    """[This function is linked and is called to from search_button.
+        it is responsible for searching for the username and password
+        for the specified web entry. if no web entry is not present the
+        function gives out an error. Finally, if there is no file the
+        function creates the file. If there is no detail in the database
+        then return.
+            Args:
+                web_entry ([object]): [user entry for the website.]
+    """
+    # get the data from the web_entry object
     website = web_entry.get()
     try:
         opened_file = open("data/password_data.json", "r")
